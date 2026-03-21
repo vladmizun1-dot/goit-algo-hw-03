b97fef636b0ada3c86e5a7eff3e4dfa078ea3529
vladm <vladmizun1@gmail.com>
2026-03-21 10:22:13 +0100

    HW3 виправлена

--- Task_2.py
+++ Task_2.py
@@ -1,11 +1,11 @@
 import random 
 def get_numbers_ticket(min, max, quantity):
-    if not (1 <= min <= max <= 1000) or not (min <= quantity <= max):       # Перевірка валідності параметрів згідно з умовами
+    if not (1 <= min <= max <= 100) or not (min <= quantity <= max):       # Перевірка валідності параметрів згідно з умовами
         return []
     
     numbers = random.sample(range(min, max + 1), quantity)  # генерує унікальні випадкові числа в діапазоні від min до max включно, кількість яких дорівнює quantity
-    # Повертаємо відсортований список
-    return sorted(numbers)
+    
+    return sorted(numbers)   # Повертаємо відсортований список
 
-print(f"Лотерейні числа: {get_numbers_ticket(19, 3, 26)}")   # Приклад виклику функції
+print(f"Лотерейні числа: {get_numbers_ticket(1, 100, 5)}")   # Приклад виклику функції
 
